a0dafbab2ddbcf20e417c5827472ffb4f770246e
askar420 <78757678+askar420@users.noreply.github.com>
2021-02-08 21:16:36 +0200

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+token = "1654259276:AAE3yNYxfrM7HFoZfG8HzxLwSLGer0EZkbw"
+token2 = '1654259276:AAE3yNYxfrM7HFoZfG8HzxLwSLGer0EZkbw'
+
+admin_id = 1364226219
+admin_id = 1364226219
+
+town = ""
+
+region = ""
+
+tovar = ""
+
+volume = ""
+
+
+a = 1364226219
+
+number = 0
